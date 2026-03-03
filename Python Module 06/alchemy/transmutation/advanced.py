4a3082059b550b4a43f2d3d1717d21d353d8a025
Yasir <y.asirbelmaalem30@gmail.com>
2026-03-03 09:55:09 +0100

    Module 06 - Finished

--- Python Module 06/alchemy/transmutation/advanced.py
+++ Python Module 06/alchemy/transmutation/advanced.py
@@ -4,7 +4,7 @@ from ..potions import healing_potion
 
 def philosophers_stone() -> str:
     return f"Philosopher's stone created using: {lead_to_gold()} " \
-           f"  and {healing_potion()}"
+           f" and {healing_potion()}"
 
 
 def elixir_of_life() -> str:
